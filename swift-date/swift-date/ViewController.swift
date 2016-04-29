3008e83c93e4d2bd243c905233d3144aacc4ac1e
xzl <xzl@sequ.biz>
2016-04-29 08:41:40 +0800

    init swift-date project

---
+++ swift-date/swift-date/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-date
+//
+//  Created by 熊智亮 on 16/4/29.
+//  Copyright © 2016年 熊智亮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
